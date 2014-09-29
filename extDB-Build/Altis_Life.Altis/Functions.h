925a14a8370f3784dda3d88e2f536e97310a924b
Patrick Maximilian Reister <patrickreister@googlemail.com>
2014-09-29 09:44:19 +0000

    Revert "rework of save and load gear"
    This reverts commit 4cf185029179fa7f7ff3958fa8d6ee3f772e3109.

--- extDB-Build/Altis_Life.Altis/Functions.h
+++ extDB-Build/Altis_Life.Altis/Functions.h
@@ -216,6 +216,7 @@ class Life_Client_Core
 		class actionKeyHandler {};
 		class playerCount {};
 		class fetchDeadGear {};
+		class loadDeadGear {};
 		class isnumeric {};
 		class escInterupt {};
 		class onTakeItem {};
